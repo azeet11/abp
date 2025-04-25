bd83bc87362bf30f7e7a601e6f71785defd58062
Ajit Nakarmi <ajitnakarmi@gmail.com>
2025-04-25 10:53:44 +0545

    Add IProjectAppService and ITasksAppService interfaces
    Introduced `IProjectAppService` and `ITasksAppService` in the
    `TaskTimeTracker.Interfaces` namespace. These interfaces provide
    asynchronous CRUD operations and support paged, filtered data
    retrieval for projects and tasks. Updated `GetListAsync` to
    accept `PagedAndFilteredResultRequestDto`. Added necessary
    `using` directives for required namespaces.

---
+++ src/TaskTimeTracker.Application/Interfaces/IProjectAppService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using TaskTimeTracker.DTOs;
+
+namespace TaskTimeTracker.Interfaces;
+
+public interface IProjectAppService
+{
+    Task<ProjectDto> CreateAsync(ProjectDto project);
+    Task<List<ProjectDto>> GetListAsync(PagedAndFilteredResultRequestDto input); // Updated method signature
+    Task<ProjectDto> GetAsync(Guid id);
+    Task<ProjectDto> UpdateAsync(Guid id, ProjectDto project);
+    Task DeleteAsync(Guid id);
+}
+
---
+++ src/TaskTimeTracker.Application/Interfaces/ITasksAppService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTimeTracker.DTOs;
+
+namespace TaskTimeTracker.Interfaces;
+
+public interface ITasksAppService
+{
+    Task<TaskDto> CreateAsync(TaskDto task);
+    Task<List<TaskDto>> GetListAsync(PagedAndFilteredResultRequestDto input);
+    Task<TaskDto> GetAsync(Guid id);
+    Task<TaskDto> UpdateAsync(Guid id, TaskDto task);
+    Task DeleteAsync(Guid id);
+}
